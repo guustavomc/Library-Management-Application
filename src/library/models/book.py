a70b2ac93c86a9179fb211695d762494697670c6
guustavomc <gustavomccb@gmail.com>
2025-10-08 19:29:17 -0300

    created initial book model and inventory book register

--- src/library/models/book.py
+++ src/library/models/book.py
@@ -1,5 +1,11 @@
 
 class Book:
-    title:str
-    author:str
-    pages:int
+    def __init__(self, title='', author='', pages=0):
+        self.title=title
+        self.author=author
+        self.pages=pages
+        self.price=0
+
+    def __str__(self):
+        return f'{self.title} | {self.author} | {self.pages} | {self.price}'
+    
--- src/library/models/inventory.py
+++ src/library/models/inventory.py
@@ -1,3 +1,25 @@
 from .book import Book
 class Inventory:
-    book:Book
+    def __init__(self):
+        self.books = []
+
+    def register_book(self, title, author, pages, price):
+        new_Book=Book()
+        new_Book.title=title
+        new_Book.author=author
+        new_Book.pages=pages
+        new_Book.price=price
+
+        self.books.append(new_Book)
+
+        return new_Book
+    
+    def display_books(self):
+        for book in self.books:
+            print(book)
+
+inventory=Inventory()
+inventory.register_book("LORD OF THE RINGS", "J R R TOLKIEN", 1000, 50)
+inventory.register_book("THE HOBBIT", "J R R TOLKIEN", 500, 30)
+
+inventory.display_books()
