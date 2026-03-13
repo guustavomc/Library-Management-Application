bae1dcb5cb19a498453dc9c6b910051c7e5f2136
guustavomc <gustavomccb@gmail.com>
2026-03-12 22:29:12 -0300

    Add unit tests for book creation, update, and deletion functionalities

--- tests/test_main.py
+++ tests/test_main.py
@@ -43,3 +43,51 @@ def test_get_book_with_id_return_book(client):
     response = client.get(f"/books/{book_created["id"]}")
     assert response.status_code == 200
     assert response.json()["id"] == book_created["id"]
+
+def test_get_book_with_id_return_not_found(client):
+    book_created =client.post("/books/", json={"name": "Dune", "author": "Frank Herbert", "pages": 412, "price": 19.99, "book_edition": 1}).json()
+    response = client.get(f"/books/fakeID")
+    assert response.status_code == 404
+
+# POST /books/
+
+def test_create_book_return_created_books(client):
+    response = client.post("/books/", json={"name": "Dune", "author": "Frank Herbert", "pages": 412, "price": 19.99, "book_edition": 1})
+    assert response.status_code == 200
+    assert response.json()["name"] == "Dune"
+
+def test_create_book_contains_id(client):
+    response = client.post("/books/", json={"name": "Dune", "author": "Frank Herbert", "pages": 412, "price": 19.99, "book_edition": 1})
+    assert response.status_code == 200
+    assert response.json()["id"] != None
+
+# PUT /books/{book_id}
+
+def test_update_book_returns_updated_data(client):
+    created = client.post("/books/", json={"name": "Dune", "author": "Frank Herbert", "pages": 412, "price": 19.99, "book_edition": 1}).json()
+    updated = client.put(f"/books/{created['id']}", json={"name": "Dune", "author": "Frank Herbert", "pages": 412, "price": 19.99, "book_edition": 2})
+    assert updated.status_code == 200
+    assert updated.json()["book_edition"] == 2
+
+def test_update_book_returns_not_found(client):
+    created = client.post("/books/", json={"name": "Dune", "author": "Frank Herbert", "pages": 412, "price": 19.99, "book_edition": 1}).json()
+    updated = client.put("/books/fakeID", json={"name": "Dune", "author": "Frank Herbert", "pages": 412, "price": 19.99, "book_edition": 2})
+    assert updated.status_code == 404
+
+# DELETE /books/{book_id}
+def test_delete_book_returns_no_content(client):
+    created = client.post("/books/", json={"name": "Dune", "author": "Frank Herbert", "pages": 412, "price": 19.99, "book_edition": 1}).json()
+    deleted = client.delete(f"/books/{created['id']}")
+    assert deleted.status_code == 204
+
+def test_delete_book_returns_not_found(client):
+    created = client.post("/books/", json={"name": "Dune", "author": "Frank Herbert", "pages": 412, "price": 19.99, "book_edition": 1}).json()
+    deleted = client.delete("/books/fakeID")
+    assert deleted.status_code == 404
+
+def test_delete_book_removes_from_book_list(client):
+    created = client.post("/books/", json={"name": "Dune", "author": "Frank Herbert", "pages": 412, "price": 19.99, "book_edition": 1}).json()
+    deleted = client.delete(f"/books/{created['id']}")
+    response = client.get("/books/")
+    assert response.status_code == 200
+    assert len(response.json()) == 0
